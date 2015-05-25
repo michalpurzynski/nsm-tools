e5a2b955d2bad34aa7acd5f3737641afc73920a1
Michal Purzynski <michal@mozilla.com>
2015-05-25 18:19:26 +0200

    Use a few CSV files instead of JSON. Fix a bug where ioc with multiple categories would appear only once. Fix a bug with no top level directory used for writes.

--- iqrisktobrosuri.py
+++ iqrisktobrosuri.py
@@ -9,45 +9,75 @@
 import os, sys
 import json
 import urllib.request
+import csv
+import io
+from dateutil.parser import parse
+import datetime
+import ipaddress
 
-wantedcats = { 'CnC':100, 'Drop':100, 'DriveBySrc':100, 'Compromised':100, 'FakeAV':100, 'Blackhole':100, 'P2PCnC':100, 'Mobile_CnC':100, 'Abused TLD':100, 'SelfSignedSSL':100, 'Bitcoin_Related':100 }
+wantedcats = { 'CnC':100, 'Drop':100, 'DriveBySrc':100, 'Compromised':100, 'FakeAV':100, 'Blackhole':100, 'P2PCnC':100, 'Mobile_CnC':100, 'Abused TLD':100, 'SelfSignedSSL':100 }
 
-headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:35.0) Gecko/20150101 Firefox/35.0',
+headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20150507 Firefox/37.0',
             'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language' : 'en-US,en;q=0.5',
             'Accept-Encoding' : 'identify',
             'Connection' : 'keep-alive' }
 
+top_path = '/var/www'
+
 def fetch_list(url):
 
     request = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(request) as data:
-        return data.read().decode('utf-8')
+        return io.StringIO(data.read().decode('utf-8'))
 
-def filter_list(ioc_list):
+def filter_list_csv(ioc_list):
 
     url = 'https://rules.emergingthreatspro.com/<code>/reputation/categories.txt'
-    filename = 'iqrisk.ioc.cat.suricata'
+    filename = top_path + '/' + 'iqrisk.ioc.cat.suricata'
 
     filtered_ioc_list = {}
     categories_to_num = {}
+    categories_to_names = {}
 
     request = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(request) as data:
         with open(filename, 'w') as f:
-            #f.write(data.read().decode('ascii', 'ignore'))
             for line in data.readlines():
                 f.write(line.decode('ascii','ignore'))
                 (catnum, catname, catdesc) = line.decode('ascii','ignore').split(',')
                 categories_to_num[catname] = catnum
+                categories_to_names[str(catnum)] = catname
 
-    for indicator, data in ioc_list.items():
-        for category, score in data.items():
-            if category in wantedcats:
-                if int(score) > int(wantedcats[category]):
-                    filtered_ioc_list[indicator] = { 'desc': category + '_' + score, 'score': score, 'catnum': categories_to_num[category] }
+    now = datetime.datetime.now()
+    iocreader = csv.DictReader(ioc_list, delimiter=',', dialect='unix')
+    for line in iocreader:
+        # {' category': '29', ' ports (|)': '443 4444 5022', 'ip': '1.1.1.3', ' score': '120', ' first_seen': '2012-05-16', ' last_seen': '2015-01-20'}
+        # yes, spaces, spaces everywhere!!
+        category = line[' category']
+        if category in categories_to_names:
+            category_name = categories_to_names[category]
+        else:
+            category_name = category
+        score = line[' score']
+        lastseen = line[' last_seen']
+        if 'ip' in line:
+            ioc_type = 'ip'
+        elif 'domain':
+            ioc_type = 'domain'
+        indicator = line[ioc_type]
+        if category_name in wantedcats:
+            if int(score) > int(wantedcats[category_name]):
+                days_from_last_seen = now - parse(lastseen)
+                if not days_from_last_seen.days > 5:
+                    if not indicator in filtered_ioc_list:
+                        filtered_ioc_list[indicator] = {}
+                    filtered_ioc_list[indicator][category] = { 'desc': category_name + '_' + score, 'score': score, 'lastseen': lastseen }
 
     f.close()
+    for okey, odict in filtered_ioc_list.items():
+        for ikey, idict in odict.items():
+            print(okey, ikey, idict)
 
     return filtered_ioc_list
 
@@ -60,10 +90,11 @@ def write_bro_intel(data, listtype):
         filename = 'iqrisk.ioc.domain.bro'
         ioctype = 'Intel::DOMAIN'
 
-    with open(filename, 'w') as f:
+    with open(top_path + '/' + filename, 'w') as f:
         print('#fields\tindicator\tindicator_type\tmeta.source\tmeta.desc\tmeta.url', file=f)
-        for indicator, details in data.items():
-            print('\t'.join([indicator, ioctype, details['desc'], 'IQRisk', 'http://www.emergingthreats.com']), file=f)
+        for indicator, outdict in data.items():
+            for catnum, indict in outdict.items():
+                print('\t'.join([indicator, ioctype, indict['desc'], 'IQRisk', 'http://www.emergingthreats.com']), file=f)
 
     f.close()
 
@@ -71,21 +102,21 @@ def write_suricata_intel(data):
 
     filename = 'iqrisk.ioc.addr.suricata'
 
-    with open(filename, 'w') as f:
-        for indicator, details in data.items():
-            print(','.join([indicator, details['catnum'], details['score']]), file=f)
+    with open(top_path + '/' + filename, 'w') as f:
+        for indicator, outdict in data.items():
+            for catnum, indict in outdict.items():
+                print(','.join([indicator, catnum, indict['score']]), file=f)
 
     f.close()
 
 def main():
 
-    urls = { 'domain': 'https://rules.emergingthreatspro.com/<code>/reputation/domainrepdata.json',
-            'addr': 'https://rules.emergingthreatspro.com/<code>/reputation/iprepdata.json' }
+    urls = { 'domain': 'https://rules.emergingthreatspro.com/<code>/reputation/detailed-domainrepdata.txt',
+            'addr': 'https://rules.emergingthreatspro.com/<code>/reputation/detailed-iprepdata.txt' }
 
     for listtype, url in urls.items():
         data = fetch_list(url)
-        json_data = json.loads(data)
-        filtered_ioc_list = filter_list(json_data)
+        filtered_ioc_list = filter_list_csv(data)
         write_bro_intel(filtered_ioc_list, listtype)
 
     write_suricata_intel(filtered_ioc_list)
